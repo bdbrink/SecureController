da3ccb974432b1c1d20c2827ec98fe2d6d5bca77
bdbrink <brendan.brink@outlook.com>
2024-06-10 16:59:27 -0500

    fixing import issues

--- main.go
+++ main.go
@@ -6,40 +6,45 @@ import (
 	"fmt"
 	"path/filepath"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
+    var kubeconfig *string
+    if home := homedir.HomeDir(); home != "" {
+        kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+    } else {
+        kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+    }
+    flag.Parse()
 
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err.Error())
-	}
+    // Build the config from the kubeconfig file
+    config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
+    if err != nil {
+        panic(err.Error())
+    }
 
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
+    // Create the clientset
+    clientset, err := kubernetes.NewForConfig(config)
+    if err != nil {
+        panic(err.Error())
+    }
 
-	admissionControllers, err := clientset.AdmissionregistrationV1().ValidatingWebhookConfigurations().List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
+    // Get the admission controllers
+    admissionControllers, err := clientset.AdmissionregistrationV1().ValidatingWebhookConfigurations().List(context.TODO(), metav1.ListOptions{})
+    if err != nil {
+        panic(err.Error())
+    }
 
-	fmt.Println("Validating Webhook Configurations:")
-	for _, webhook := range admissionControllers.Items {
-		fmt.Printf("- Name: %s\n", webhook.Name)
-		for _, rule := range webhook.Webhooks {
-			fmt.Printf("  Webhook: %s\n", rule.Name)
-		}
-	}
+    // Print the admission controllers
+    fmt.Println("Validating Webhook Configurations:")
+    for _, webhook := range admissionControllers.Items {
+        fmt.Printf("- Name: %s\n", webhook.Name)
+        for _, rule := range webhook.Webhooks {
+            fmt.Printf("  Webhook: %s\n", rule.Name)
+        }
+    }
 }
